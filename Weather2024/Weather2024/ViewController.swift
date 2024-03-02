3bd89a489fba7459cd21caad59b48851a02432fd
hakkicansengonul <hakkicansengonul@gmail.com>
2024-03-02 16:35:01 +0300

    Setup

---
+++ Weather2024/Weather2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather2024
+//
+//  Created by Hakkı Can Şengönül on 2.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
